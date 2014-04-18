d0e32c74d0ca4010a42b15cb5b33db23a4c26ac5
Peter Ruibal <ruibalp@gmail.com>
2014-04-18 01:56:04 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from sparts.vservice import VService
+from sparts.tasks.tornado import TornadoHTTPTask
+
+from tornado.web import RequestHandler
+
+class WebApp(TornadoHTTPTask):
+    class RedirectToGithub(RequestHandler):
+        def initialize(self, task):
+            self.task = task
+
+        def get(self, trailer=None):
+            self.task.logger.info("GET /%s", trailer)
+            self.redirect('https://github.com/fmoo/sparts')
+
+    def getApplicationConfig(self):
+        return [
+            ('/(.*)', self.RedirectToGithub, {'task': self})
+        ]
+
+WebApp.register()
+
+if __name__ == '__main__':
+    VService.initFromCLI()
